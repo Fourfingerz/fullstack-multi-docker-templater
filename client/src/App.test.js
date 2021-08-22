07326ff7260b19f01f03288f39a2d9b0e76b4731
Fourfingerz <chansd5@gmail.com>
2021-08-22 12:41:36 -0400

    Initial Commit

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  // USE A FAKER MODULE TO MOCK BACKEND REDIS and PG CALLS
+});
